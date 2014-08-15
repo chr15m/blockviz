44fc9084e5f72e5abc7e1ace96e54593fc50669e
Chris McCormick <chris@mccormick.cx>
2014-08-15 23:50:51 +0200

    Fetch a list of addresses from blockchain.info and manage the address data, maintaining the list the user cares about with the 'use' flag.

--- data.js
+++ data.js
@@ -17,3 +17,31 @@ function parseAddresses(text) {
 	});
 	return {"addresses": addresses, "rejected": rejected};
 }
+
+var known_addresses = {};
+
+// update the list of addresses we know about with new address data
+function updateKnownAddresses(new_addresses) {
+	for (var a=0; a<new_addresses.length; a++) {
+		known_addresses[new_addresses[a].address] = new_addresses[a];
+		known_addresses[new_addresses[a].address].use = true;
+	}
+}
+
+// return a list of addresses we don't know about yet
+function filterKnownAddresses(new_addresses) {
+	return _.filter(new_addresses, function(e, i) {
+		return known_addresses[e] ? null : e;
+	});
+}
+
+// mark a particular address as in the user's desired visible list
+function markKnownAddresses(addresses) {
+	for (var a in known_addresses) {
+		if (addresses.indexOf(a) != -1) {
+			known_addresses[a].use = true;
+		} else {
+			known_addresses[a].use = false;
+		}
+	}
+}
--- ui.js
+++ ui.js
@@ -1,6 +1,24 @@
 $(function() {
-	// hide the addresses to begin with
-	$("#addresses").hide();
+	// when we first launch, check what addresses to load
+	var q = document.location.href.split("?")[1];
+	if (q) {
+		// if there is a comma separated list of addresses passed in
+		if (q.indexOf(",") != -1) {
+			$("#addresses").text(q.replace(/\,/g, "\n"));
+			updateFromAddressBox();
+		// if there is a URL passed in
+		/*} else if (q.indexOf("http")) {
+			$.get(q, function(data) {
+				console.log(data);
+			}, "text");*/
+		// if there is a textfile name passed in
+		} else {
+			$.get("addresses/" + q + ".txt", function(data) {
+				$("#addresses").text(data);
+				updateFromAddressBox();
+			}, "text");
+		}
+	}
 	
 	// hide the ajax spinner now that we are running
 	$("#loader").hide();
@@ -23,33 +41,57 @@ $(function() {
 		if (timer) {
 			clearTimeout(timer);
 		}
-		var element = this;
 		timer = setTimeout(updateFromAddressBox, 500);
 	});
-	
-	// when we first launch, check what addresses to load
-	var q = document.location.href.split("?")[1];
-	if (q) {
-		// alert(q);
-		if (q.indexOf(",")) {
-		}
-	}
 });
 
 // parse addresses out of the address box and fetch them
 function updateFromAddressBox() {
-	function () {
-		$("#log").html("");
-		var parsed = parseAddresses($(element).val());
-		// display rejected addresses
-		if (parsed.rejected.length) {
-			$("#log").append("Rejected addresses:<br/>");
-			for (var r=0; r<parsed.rejected.length; r++) {
-				var escaped = parsed.rejected[r].replace(/&/g, '&amp;').replace(/"/g, '&quot;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
-				$("#log").append(escaped + "<br/>");
-			}
+	$("#log").html("");
+	$("#loading").show();
+	$("#loading").text("Parsing addresses.");
+	var parsed = parseAddresses($("#addresses").val());
+	// display rejected addresses
+	if (parsed.rejected.length) {
+		$("#log").append("Rejected addresses:<br/>");
+		for (var r=0; r<parsed.rejected.length; r++) {
+			var escaped = parsed.rejected[r].replace(/&/g, '&amp;').replace(/"/g, '&quot;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+			$("#log").append(escaped + "<br/>");
 		}
-		// start fetching the addresses from blockchain.info
-		fetchAddresses();
+	}
+	// fetch good addresses
+	if (parsed.addresses.length) {
+		$("#loading").text("Fetching " + parsed.addresses.length + " addresses.");
+		markKnownAddresses(parsed.addresses);
+		fetchAddresses(parsed.addresses);
+	} else {
+		$("#log").append("No good addresses found.<br/>");
+		$("#loading").hide();
+	}
+}
+
+// fetch all of the requested addresses from blockchain.info
+function fetchAddresses(addresses) {
+	var addresses = filterKnownAddresses(addresses);
+	var address_count = addresses.length;
+	// fetch the addresses in blocks of 20
+	var jump = 20;
+	var address_blocks = Math.ceil(addresses.length / jump);
+	for (var b=0; b<address_blocks; b++) {
+		$.getJSON("https://blockchain.info/multiaddr?cors=true&active=" + addresses.slice(b * jump, (b+1) * jump).join("|"), function(data) {
+			console.log(data.addresses);
+			updateKnownAddresses(data.addresses);
+			address_count -= data.addresses.length;
+			$("#loading").text("Fetching " + address_count + " addresses.");
+			checkAddressCount(address_count);
+		});
+	}
+	checkAddressCount(address_count);
+}
+
+// if addresses-to-be-fetched count has reached zero, hide log
+function checkAddressCount(address_count) {
+	if (address_count == 0) {
+		$("#loading").hide();
 	}
 }
